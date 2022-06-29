aa3a594bba3faac037d1ba734aaeee0f40d65357
Yeray Gonzalez bartolome <ygonzale@c4r7s2.42urduliz.com>
2022-06-29 16:16:01 +0200

    errors

--- check_errors.c
+++ check_errors.c
@@ -6,7 +6,7 @@
 /*   By: ygonzale <ygonzale@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/29 13:44:50 by ygonzale          #+#    #+#             */
-/*   Updated: 2022/06/29 15:57:49 by ygonzale         ###   ########.fr       */
+/*   Updated: 2022/06/29 16:15:43 by ygonzale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,9 @@ check_errors(int argc, char **argv)
 {
 	int i;
 
-	if ()
+	if (!argc == 2)
+	{
+		printf("Number of argument invalid\n");
+		exit(0);
+	}
 }
