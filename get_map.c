164612371c7675de0766dec8f5495060a298a7d1
Yeray Gonzalez bartolome <ygonzale@c4r8s6.42urduliz.com>
2022-07-01 15:37:42 +0200

    progress geting map

--- get_map.c
+++ get_map.c
@@ -6,7 +6,7 @@
 /*   By: ygonzale <ygonzale@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 10:56:18 by ygonzale          #+#    #+#             */
-/*   Updated: 2022/07/01 13:50:39 by ygonzale         ###   ########.fr       */
+/*   Updated: 2022/07/01 15:37:35 by ygonzale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,21 +20,34 @@
 	ft_strlen;
 } */
 
-void	ft_map(t_game *game, char **argv)
+int	count_lines(char **argv)
 {
-	int	lines;
-	int	fd;
+	char	*line;
+	int		fd;
+	int		count;
 
+	count = 0;
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 		ft_putendl_fd("Error to open map", 2);
 	else
 	{
-		lines = get_next_line(fd);
-		while (lines)
+		line = get_next_line(fd);
+		while (line)
 		{
-			
+			count++;
+			free(line);
+			line = get_next_line(fd);
 		}
+		close(fd);
 	}
-	game->map.map = malloc();
+	return (count++);
+}
+
+void	ft_map(t_game *game, char **argv)
+{
+	int	lines;
+
+	lines = count_line(argv);
+	game->map.map = malloc(sizeof(char *) * lines + 1);
 }
