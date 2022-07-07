1a83e28e7bc407f0050f472ccfbde7134b399b72
Yeray Gonzalez bartolome <ygonzale@c3r4s4.42urduliz.com>
2022-07-07 16:23:49 +0200

    mapping

--- get_map.c
+++ get_map.c
@@ -6,7 +6,7 @@
 /*   By: ygonzale <ygonzale@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 10:56:18 by ygonzale          #+#    #+#             */
-/*   Updated: 2022/07/05 13:51:29 by ygonzale         ###   ########.fr       */
+/*   Updated: 2022/07/07 16:19:30 by ygonzale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,18 +41,22 @@ int	count_lines(char **argv)
 
 void	check_map(t_program *program, char **argv)
 {
-	int	fd;
-	int size;
+	int		fd;
+	char	size;
+	char	**split_map;
 
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 		return (0);
 	program->map->map = malloc(sizeof(char **) * 1);
-	if (!program->map->map)
+	if (!(program->map)->map)
 		return (0);
 	size = get_next_line (fd);
-	program->map->row = ft_strlen(program->map->map[0]);
+	split_map = ft_split(size, "\n");
+	free(size);
+	program->map->row = ft_strlen(split_map[0]);
 	program->map->col = count_lines(argv);
+	close(fd);
 }
 
 void	ft_map(t_program *program, char **argv)
@@ -72,5 +76,5 @@ void	ft_map(t_program *program, char **argv)
 		free_double_array(program->map->map);
 		return ;
 	}
-	check_map(&program, argv);
+	check_map(program, argv);
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ygonzale <ygonzale@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/29 11:17:28 by ygonzale          #+#    #+#             */
-/*   Updated: 2022/07/05 12:38:29 by ygonzale         ###   ########.fr       */
+/*   Updated: 2022/07/07 16:20:08 by ygonzale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 int	main(int argc, char **argv)
 {
-	t_program	*s_program;
+	t_program	s_program;
 
 	check_errors(argc, argv);
-	ft_map(s_program, argv);
-	s_program->mlx_pointer = mlx_init();
-	s_program->format->format = mlx_new_window(s_program->mlx_pointer, \
-			s_program->map->row * 64, s_program->map->col * 64, "POKEMON'T");
+	ft_map(&s_program, argv);
+	s_program.mlx_pointer = mlx_init();
+	s_program.format->format = mlx_new_window(s_program.mlx_pointer, \
+		s_program.map->row * 64, s_program.map->col * 64, "POKEMON'T");
 }
