876f8fec45e5b6080068cafccc8b0a45acece450
Yeray Gonzalez bartolome <ygonzale@c3r1s4.42urduliz.com>
2022-09-09 13:59:52 +0200

    wip sprites

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ygonzale <ygonzale@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/29 11:17:28 by ygonzale          #+#    #+#             */
-/*   Updated: 2022/09/09 13:14:23 by ygonzale         ###   ########.fr       */
+/*   Updated: 2022/09/09 13:59:13 by ygonzale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 int	main(int argc, char **argv)
 {
-	t_program	s_program;
+	t_program	program;
 
 	check_argc_errors(argc);
-	ft_map(&s_program, argv);
-	s_program.mlx_pointer = mlx_init();
-	s_program.format.format = mlx_new_window(s_program.mlx_pointer, \
-		s_program.map->length * 64, s_program.map->height * 64, "POKEMON'T");
-	get_sprites(&s_program);
-	mlx_loop(s_program.mlx_pointer);
+	ft_map(&program, argv);
+	program.mlx_pointer = mlx_init();
+	program.format.format = mlx_new_window(program.mlx_pointer, \
+		program.map->length * 64, program.map->height * 64, "POKEMON'T");
+	get_sprites(&program);
+	mlx_loop(program.mlx_pointer);
 }
