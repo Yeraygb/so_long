9f8f54d9754103bf04d5541f600f1e42dfad7c5e
Yeray Gonzalez bartolome <ygonzale@c3r6s2.42urduliz.com>
2022-09-22 14:51:05 +0200

    done

--- include/so_long.h
+++ include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: ygonzale <ygonzale@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/29 11:34:57 by ygonzale          #+#    #+#             */
-/*   Updated: 2022/09/22 14:25:26 by ygonzale         ###   ########.fr       */
+/*   Updated: 2022/09/22 14:50:25 by ygonzale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,13 +81,14 @@ typedef struct s_program
 	t_counter	counter;
 }	t_program;
 
-void	check_argc_errors(int argc);
+// ---------- Libft ---------- //
+
+char	**ft_split(char const *s, char c);
+void	ft_putendl_fd(char *s, int fd);
+
 void	ft_map(t_program *program, char **argv);
 void	free_double_array(char **trash);
 int		count_lines(char **argv, t_program *program);
-void	check_map(t_program *program);
-char	**ft_split(char const *s, char c);
-void	ft_putendl_fd(char *s, int fd);
 void	get_sprites(t_program *program);
 void	save_images(t_program *program);
 void	sprites_to_windows(t_program *program, int x, int y);
@@ -96,8 +97,13 @@ void	exit_program(t_program *program);
 int		horizontal(int keycode, t_program *program, int x, int y);
 int		vertical(int keycode, t_program *program, int x, int y);
 int		move(t_program *program, int x, int y);
-void	error_char_map(t_program *program);
 void	counts(t_program *program);
+
+// ---------- Errors ---------- //
+
+void	check_argc_errors(int argc);
+void	check_map(t_program *program);
+void	error_char_map(t_program *program);
 int		check_collects(t_program *program);
 void	check_ber(char **argv);
 void	check_rectangle(t_program *program);
